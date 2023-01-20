a66d37654ede21cc6da592326c4f27995ee95ae8
b4kii <dami4nhalibart@gmail.com>
2023-01-20 17:02:35 +0100

    add mobile styles for loading page

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,30 +1,13 @@
-// import zIndex from '@mui/material/styles/zIndex'
 import React from "react";
 import { Icon } from "@iconify/react";
-import { lineHeight } from "@mui/system";
 
 export default function LoadingScreen() {
-  const styles = {
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    background: "whitesmoke",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    zIndex: "100",
-    gap: "1rem",
-    // overflow: "hidden"
-  };
   return (
-    <div style={styles}>
-      <span style={{ fontSize: "3rem" }}>Please wait </span>
+    <div className="loading">
+      <span >Please wait </span>
       <Icon
         icon="svg-spinners:3-dots-bounce"
-        style={{ fontSize: "5rem", marginTop: ".7rem"}}
-      />{" "}
+      />
     </div>
   );
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 
 export default function ErrorPage() {
-  const container = {
-    widht: "70vw",
-    height: "100vh",
-    paddingTop: "3.5rem",
-    fontSize: "2em",
-    display: "grid",
-    placeContent: "center",
-    textAlign: "center"
-  }
-
   return (
-    <section style={container}>
+    <section className="container">
       <h1>Something went wrong :-/</h1>
-      <p style={{fontSize: ".8em"}}>Please refresh the page</p>
+      <p>Please refresh the page</p>
     </section>
   )
 }
