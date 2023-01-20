bbbe04f3e2fedbf0444382211e63bd8dddeaa6cb
b4kii <dami4nhalibart@gmail.com>
2023-01-20 18:03:23 +0100

    fix class name conflict

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 
 export default function LoadingScreen() {
   return (
-    <div className="loading">
+    <div className="loading-screen">
       <span >Please wait </span>
       <Icon
         icon="svg-spinners:3-dots-bounce"
